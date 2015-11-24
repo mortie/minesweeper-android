b9c85517ad7c64bd31fe6b83c19fab3faa837bb3
mort <martid0311@gmail.com>
2015-11-24 01:58:32 +0100

    various improvements

--- app/src/main/java/coffee/mort/minesweeper/MainActivity.java
+++ app/src/main/java/coffee/mort/minesweeper/MainActivity.java
@@ -1,5 +1,6 @@
 package coffee.mort.minesweeper;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
@@ -13,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
 
         WebView view = (WebView)findViewById(R.id.webView);
         view.getSettings().setJavaScriptEnabled(true);
+        view.getSettings().setDomStorageEnabled(true);
+        view.getSettings().setDatabaseEnabled(true);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            view.getSettings().setDatabasePath("/data/data/" + view.getContext().getPackageName() + "/databases/");
+        }
         view.loadUrl("file:///android_asset/www/index.html");
     }
 }
